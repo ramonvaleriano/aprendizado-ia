17050a77c46c24b2e4e11764c79f78999b428103
Ramon Valeriano <rrvaleriano@gmail.com>
2025-10-28 21:36:01 -0300

    feat(roles.py): Tranformando em classe para ficar mais fácil o aprendizado

--- roles.py
+++ roles.py
@@ -4,22 +4,36 @@ from settings import GROQ_API
 
 client = Groq(api_key=GROQ_API)
 
-"""
-Roles: São os papeis que o agente tem.
 
-O Agente pode ser dividios em Roles e Contents, os Contents são o conteudo propriamente dito.
+class RolesGroq:
+    """
+    Roles: São os papeis que o agente tem.
 
-User: Mensagem do Usuário
-Assistant: Resposta do Modelo
-System: Intruções de comportamento para o modelo. 
+    O Agente pode ser dividios em Roles e Contents, os Contents são o conteudo propriamente dito.
 
-"""
+    User: Mensagem do Usuário
+    Assistant: Resposta do Modelo
+    System: Intruções de comportamento para o modelo.
 
-chat_completion = client.chat.completions.create(
-    messages=[{"role": "user", "content": "Olá Mundo"}],
-    model="openai/gpt-oss-120b"
-)
+    """
 
+    @property
+    def chat_simple(self):
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Me explique de forma reduzida o qu são as LLMs",
+                }
+            ],
+            model="openai/gpt-oss-120b",
+        )
+
+        return chat_completion
+
+
+roles_groq = RolesGroq()
+chat_completion = roles_groq.chat_simple
 
 print(f"Resposta completo do modelo: {chat_completion}")
 print("\n")

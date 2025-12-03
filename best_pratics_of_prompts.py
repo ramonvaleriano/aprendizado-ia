7b9761694d4bf3b4478fbfbf3770a3c68eaba75c
Ramon Valeriano <rrvaleriano@gmail.com>
2025-12-03 20:58:30 -0300

    feat(best_pratics_of_prompts.py): Validando um prompt bem vago

--- best_pratics_of_prompts.py
+++ best_pratics_of_prompts.py
@@ -15,4 +15,30 @@ class BestPraticsOfPrompts:
         self.client = Groq(api_key=self.__api_key)
 
     def first_prompt(self):
-        pass
+        # Prompt vem vago, sem especificidade.
+        prompt_vago = "Fale Sobre café"
+        response_vago = self.client.chat.completions.create(
+            model=self.__model,
+            temperature=0.7,
+            max_completion_tokens=400,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Você é um especialista em gastronomia"
+                },
+                {
+                    "role": "user",
+                    "content": prompt_vago
+                }
+            ]
+        )
+
+        response = response_vago.choices[0].message.content
+
+        print(f"Reponse: {response}")
+
+        return response
+    
+
+test_best_pratics = BestPraticsOfPrompts()
+test_best_pratics.first_prompt()

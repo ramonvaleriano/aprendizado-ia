d485dace2c066795082cfdd51b5a997e2e320e08
Ramon Valeriano <rrvaleriano@gmail.com>
2025-12-05 16:35:42 -0300

    feat(structuring_output.py): Estruturando o nosso primeiro output

--- structuring_output.py
+++ structuring_output.py
@@ -9,4 +9,42 @@ class StructuringOutput:
         self.__api_key = groq_apy_key
         self.client = Groq(api_key=groq_apy_key)
 
+    def first_output(self):
+        # Criando estrutura de saída.
+        system_prompt = """
+        Você é um historiador especialista em civilizações antigas.
+        Sempre responda no formato de tabela:
+
+        | Civilização | Período | Contribuição Marcante | Região |
+        | ----------- | ------- | --------------------- | ------ |
+        """
+
+        user_prompt = """
+        Crie uma tabela comparando 4 civilizações antigas (Egito, Mesopotâmia, Grécia, Roma).
+        """
+
+        response_output_one = self.client.chat.completions.create(
+            model=self.__model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": system_prompt
+                },
+                {
+                    "role": "user",
+                    "content": user_prompt
+                }
+            ],
+            temperature=0.5
+        )
+
+        response = response_output_one.choices[0].message.content
+
+        print("Response:")
+        print(response)
+
+        return response
     
+
+test_outputs = StructuringOutput()
+test_outputs.first_output()

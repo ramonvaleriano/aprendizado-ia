334007aa74623442fd5623e63def009737f5b988
Ramon Valeriano <rrvaleriano@gmail.com>
2025-12-08 16:01:11 -0300

    feat(few_shop_promptintg.py): Usando few shot no prompt

--- few_shop_promptintg.py
+++ few_shop_promptintg.py
@@ -71,11 +71,45 @@ class FewShopPrompt:
         print(f"Response: \n{response}")
 
         return response
+    
+    async def few_shot_prompt(self):
+        system_prompt = """
+        Você é um chef renomado.
+        Receba o nome de um ingrediante e sugira um prato sofisticado onde ele seja protagonista.
+
+        <exemplos>
+        Ingrediente: Tomate
+        Sugestão <-> Carpaccio de tomate com azeite frufado e manjericão fresco
+
+        Ingrediente: Chocolate
+        Sugestão <-> Soufflé de chocolate meio amargo com calda de frutas vermelhas
+
+        Ingrediente: Batata
+        Sugestão <-> Nhoque artesanal de batata ao molho de manteiga e sálvia.
+        </exemplos>
+        """
+
+        user_prompt = "Camarão"
+
+        response_complete = self.client.chat.completions.create(
+            model=self.__model,
+            temperature=0.5,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+        )
+
+        response = response_complete.choices[0].message.content
+
+        print(f"Response: \n{response}")
+
+        return response
 
 
 async def test_few_shot_prompt():
     few_shot_prompt = FewShopPrompt()
-    await few_shot_prompt.one_shot_prompt()
+    await few_shot_prompt.few_shot_prompt()
 
 
 asyncio.run(test_few_shot_prompt())

4c61821328074bf8050a68b420001bac3c3dcab3
Ramon Valeriano <rrvaleriano@gmail.com>
2026-02-11 17:07:54 -0300

    feat(agentes_de_ia_zero_ao_avancado/a_engenharia_de_prompt/engenharia_de_prompt/best_pratics.py): Deixando o prompot mais especifico

--- agentes_de_ia_zero_ao_avancado/a_engenharia_de_prompt/engenharia_de_prompt/best_pratics.py
+++ agentes_de_ia_zero_ao_avancado/a_engenharia_de_prompt/engenharia_de_prompt/best_pratics.py
@@ -40,8 +40,31 @@ class BestPratics:
         print(response)
 
         return response
+    
+    async def best_pratics_second(self):
+        """Prompt vago"""
+        system_prompt = "Você é um especialista em gastronimia."
+        user_prompt = "Liste e expique 3 métodos de preparo de café que realçam diferentes sabores."
+
+        response_complete = self.client.chat.completions.create(
+            model=self.__model,
+            temperature=self.temperature,
+            max_completion_tokens=800,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+        )
+
+        response = response_complete.choices[0].message.content
+
+        print(f"Pergunta: {user_prompt}")
+        print("Reponse: ")
+        print(response)
+
+        return response
 
 
 async def run_main():
     best_pratics = BestPratics()
-    await best_pratics.best_pratics_first()
+    await best_pratics.best_pratics_second()

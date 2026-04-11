19c3979f6ff9cbeb3411758346cf8cf4d05670bc
Ramon Valeriano <rrvaleriano@gmail.com>
2026-04-10 22:37:12 -0300

    feat(): Começando a mexer com IA de fato

---
+++ python-inteligencia-artificil-aplicada/manipulando_string.py
@@ -0,0 +1,14 @@
+"""
+    Aprendendo a conectar a IA do google cm Python
+"""
+from setting_ia import GEMINI_API_KEY
+from google import genai
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents="Que tipo de IA é você?"
+)
+
+print(response.text
--- python-inteligencia-artificil-aplicada/setting_ia.py
+++ python-inteligencia-artificil-aplicada/setting_ia.py
@@ -3,6 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API = os.getenv("GROQ_API", None)
-MODEL_START = os.getenv("MODEL_START", None)
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", None)
